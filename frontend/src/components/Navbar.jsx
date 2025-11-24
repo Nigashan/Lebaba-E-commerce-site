0ef828330a6cdd5bfa54f0498d3101a106fd7641
Nigashan <it21026720@my.sliit.lk>
2025-11-24 19:52:59 +0530

    Navbar has been done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <header className='fixed-nav-bar w-nav'>
-        <nav>
+        <nav className='max-w-screen-2xl mx-auto px-4 flex justice-between items-center'>
             <ul className='nav__links'>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/Shop'>Shop</Link></li>
@@ -13,11 +13,31 @@ const Navbar = () => {
                 <li><Link to='/Contact'>Contact</Link></li>
             </ul>
 
-        <nav className='nav__logo'>
+        <div className='nav__logo'>
         <Link to="/">
         Lebaba<span>.</span>
         </Link>
-        </nav>
+        </div>
+
+        <div className='nav__icons relative'>
+          <span>
+            <Link to='/search'>
+            <i className="ri-search-line"></i>
+            </Link>
+          </span>
+          <span>
+            <button className='hover:text-primary'>
+              <i className='ri-shopping-bag-line'> </i>
+              <sup className='text-sm inline-block px-1.5 text-white bg-primary text-center rounded-full'>0</sup>
+            </button>
+          </span>
+
+           <span>
+            <Link to='login'>
+            <i className="ri-user-line"></i>
+            </Link>
+          </span>
+        </div>
 
         </nav>
     </header>
